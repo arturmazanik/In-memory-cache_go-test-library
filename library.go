315c1a529ba2cacc5e75235cd44d5a1ef1f23e52
Artur Mazanik <artur.mazanik@softgamings.com>
2025-12-07 00:03:11 +0300

    add worker pool

--- library.go
+++ library.go
@@ -1,6 +1,7 @@
 package inmemlib
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -49,4 +50,51 @@ func (c *Cache) Delete(key string) {
 	c.mu.Lock()
 	delete(c.storage, key)
 	c.mu.Unlock()
+}
+
+// Worker Pool
+type CacheTask struct {
+	Key   string
+	Value string
+	TTL   time.Duration
+}
+
+type WorkerPool struct {
+	Tasks      chan CacheTask
+	cache      *Cache
+	wg         sync.WaitGroup
+	numWorkers int
+}
+
+// Создать новый пул
+func NewWorkerPool(numWorkers int, cache *Cache) *WorkerPool {
+	return &WorkerPool{
+		Tasks:      make(chan CacheTask, 100),
+		cache:      cache,
+		numWorkers: numWorkers,
+	}
+}
+
+// Запустить воркеров
+func (wp *WorkerPool) Run() {
+	for i := 0; i < wp.numWorkers; i++ {
+		wp.wg.Add(1)
+		go wp.worker(i)
+	}
+}
+
+// Worker функция
+func (wp *WorkerPool) worker(id int) {
+	defer wp.wg.Done()
+	for task := range wp.Tasks {
+		// Здесь добавляем элемент в кеш
+		wp.cache.Set(task.Key, task.Value, task.TTL)
+		fmt.Printf("Worker %d set key=%s\n", id, task.Key)
+	}
+}
+
+// Ожидание завершения всех воркеров
+func (wp *WorkerPool) Wait() {
+	close(wp.Tasks)
+	wp.wg.Wait()
 }
